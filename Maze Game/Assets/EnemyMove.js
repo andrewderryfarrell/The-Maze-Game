1e8ea25026cbc5bcffe3b1eacbbdb07f26c2b9a7
Cormac McCarthy <cormacpayne@ufl.edu>
2014-04-06 15:07:19 -0400

    AWESOME STUFF!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Maze Game/Assets/EnemyMove.js
+++ Maze Game/Assets/EnemyMove.js
@@ -1,17 +1,34 @@
-﻿static var speed : int = 5;
+﻿static var speed : int = 3;
+static var player : GameObject;
+static var myTransform : Transform;
+static var target : Transform;
 
-function Update () {
-	
+function Update() {	
 	var number = parseInt(Random.Range(1, 5));
-	Debug.Log("Number: " + number);
+	// Debug.Log("Number: " + number);
+	player = GameObject.Find("Player");
 	
-	if (number == 1) {
-		transform.Translate (Vector3(0,1,0) * Time.deltaTime*speed);
-	} else if (number == 2) {
-		transform.Translate (Vector3(0,-1,0) * Time.deltaTime*speed);
-	} else if (number == 3) {
-		transform.Translate (Vector3(-1,0,0) * Time.deltaTime*speed);
-	} else {
-		transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
-	}
+	// Debug.Log(player.transform.position);
+	
+	myTransform = this.transform;
+	
+	target = player.transform;
+	
+	myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+						   Quaternion.LookRotation(target.position - myTransform.position), 
+						   Time.deltaTime * speed); 
+	
+	myTransform.position += myTransform.forward * Time.deltaTime * speed;
 }
+
+// "UP"
+// transform.Translate (Vector3(0,1,0) * Time.deltaTime*speed);
+
+// "DOWN"
+// transform.Translate (Vector3(0,-1,0) * Time.deltaTime*speed);
+
+// "LEFT"
+// transform.Translate (Vector3(-1,0,0) * Time.deltaTime*speed);
+
+// "RIGHT"
+// transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
