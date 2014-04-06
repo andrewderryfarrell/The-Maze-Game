6630bb455aeeaf124f9e987950f8e168b82dde92
Cormac McCarthy <cormacpayne@ufl.edu>
2014-04-06 13:22:52 -0400

    Player/Enemy movement

---
+++ Maze Game/Assets/EnemyMove.js
@@ -0,0 +1,17 @@
+﻿static var speed : int = 5;
+
+function Update () {
+	
+	var number = parseInt(Random.Range(1, 5));
+	Debug.Log("Number: " + number);
+	
+	if (number == 1) {
+		transform.Translate (Vector3(0,1,0) * Time.deltaTime*speed);
+	} else if (number == 2) {
+		transform.Translate (Vector3(0,-1,0) * Time.deltaTime*speed);
+	} else if (number == 3) {
+		transform.Translate (Vector3(-1,0,0) * Time.deltaTime*speed);
+	} else {
+		transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
+	}
+}
---
+++ Maze Game/Assets/PlayerMove.js
@@ -0,0 +1,21 @@
+﻿static var speed : int = 3;
+     
+function Update () {
+			
+	// Debug.Log(transform.position);	
+		
+	// Debug.Log("HERE");
+	if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
+		// Debug.Log("UP");
+		transform.Translate (Vector3(0,1,0) * Time.deltaTime*speed);
+	} else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
+		// Debug.Log("DOWN");
+		transform.Translate (Vector3(0,-1,0) * Time.deltaTime*speed);
+	} else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+		// Debug.Log("LEFT");
+		transform.Translate (Vector3(-1,0,0) * Time.deltaTime*speed);
+	} else if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+		// Debug.Log("RIGHT");
+		transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
+	}		
+}
