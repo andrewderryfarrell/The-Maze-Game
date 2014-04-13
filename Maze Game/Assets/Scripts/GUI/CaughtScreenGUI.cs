a2f1d4d671de1d24a586b2a4621b4b36100206c3
Andrew Derry-Farrell <andrewderryfarrell@users.noreply.github.com>
2014-04-13 14:09:56 -0400

    Added Start Screen and  Caught Screen
    Added Start Screen and Caught Screen.
    Organized Files

---
+++ Maze Game/Assets/Scripts/GUI/CaughtScreenGUI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaughtScreenGUI : MonoBehaviour
+{
+    public float titleLabelYAxis;
+    public float firstLineYAxis;
+    public float secondLineYAxis;
+    public float thirdLineYAxis;
+    public float retryLineYAxis;
+
+    public GUIStyle titleStyle;
+    public GUIStyle textStyle;
+    private Texture2D grayBackground;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Application.LoadLevel(0);
+        }
+        else if (Input.anyKeyDown)
+        {
+            //Get the level variable assigned in the player class and load level
+            print("You pressed a key! You want to rety the level!");
+        }
+    }
+
+    void OnGUI()
+    {
+        titleStyle = new GUIStyle(GUI.skin.label);
+        titleStyle.normal.textColor = Color.gray;
+        titleStyle.fontSize = (int)(.07f * Screen.height);
+        titleStyle.fontStyle = FontStyle.Bold;
+
+        textStyle = new GUIStyle(GUI.skin.label);
+        textStyle.normal.textColor = Color.white;
+        textStyle.fontSize = (int)(.04f * Screen.height);
+
+        
+        GUI.Label(new Rect(Screen.width * .38f, Screen.height * titleLabelYAxis, Screen.width, Screen.height * .2f), "The Maze Game", titleStyle);
+
+        GUI.Label(new Rect(Screen.width * .15f, Screen.height * firstLineYAxis, Screen.width, Screen.height * .2f), "You have been caught. To Continue:", textStyle);
+        GUI.Label(new Rect(Screen.width * .15f, Screen.height * secondLineYAxis, Screen.width, Screen.height * .2f), "Press Escape to exit game, or", textStyle);
+        GUI.Label(new Rect(Screen.width * .15f, Screen.height * thirdLineYAxis, Screen.width, Screen.height * .2f), "Press Enter to return to Main Menu.", textStyle);
+
+        GUI.Label(new Rect(Screen.width * .38f, Screen.height * retryLineYAxis, Screen.width, Screen.height * .2f), "Press any key to retry level", textStyle);
+
+        
+
+    }
+}
---
+++ Maze Game/Assets/Scripts/GUI/StartScreenGUI.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenGUI : MonoBehaviour 
+{
+    public float titleLabelYAxis;
+    public float playButtonYAxis;
+    public float loadButtonYAxis;
+    public float exitButtonYAxis;
+    
+    public GUIStyle buttonStyle;
+    public GUIStyle titleStyle;
+    private bool isFirst = true;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Application.LoadLevel(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            print("You want to load a level");
+        }
+        else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnGUI()
+    {
+        titleStyle = new GUIStyle(GUI.skin.label);
+        titleStyle.normal.textColor = Color.green;
+        titleStyle.fontSize = (int)(.16f * Screen.height);
+        titleStyle.fontStyle = FontStyle.Bold;
+
+        GUI.Label(new Rect(Screen.width * .20f, Screen.height * titleLabelYAxis, Screen.width, Screen.height * .2f), "The Maze Game", titleStyle);
+
+        //Button Style
+        buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.normal.textColor = Color.white;
+        buttonStyle.hover.textColor = Color.cyan;
+        buttonStyle.focused.textColor = Color.cyan;
+        buttonStyle.alignment = TextAnchor.MiddleCenter;
+        buttonStyle.fontSize = (int)(.07f * Screen.height);
+        buttonStyle.fontStyle = FontStyle.Italic;
+
+        //Play Game Button
+        GUI.SetNextControlName("Play");
+        if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * playButtonYAxis, Screen.width * .5f, Screen.height * .1f), "Play Game", buttonStyle))
+        {
+            //Pressed
+            Application.LoadLevel("Game");
+        }
+
+        //Load Level Button
+        GUI.SetNextControlName("Load");
+        if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * loadButtonYAxis, Screen.width * .5f, Screen.height * .1f), "Load Level", buttonStyle))
+        {
+            //Pressed
+            print("You clicked the Load Level Button!");
+        }
+
+        //Exit Game Button
+        GUI.SetNextControlName("Exit");
+        if (GUI.Button(new Rect(Screen.width * .25f, Screen.height * exitButtonYAxis, Screen.width * .5f, Screen.height * .1f), "Exit Game", buttonStyle))
+        {
+            //Pressed
+            Application.Quit();
+        }
+    }
+}
