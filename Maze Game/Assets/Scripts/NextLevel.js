c22da94abd28b6367f536473cf9156cffaa9144f
Cormac McCarthy <cormacpayne@ufl.edu>
2014-04-16 20:09:22 -0400

    added timer, fonts
    lololololololololololololol

--- Maze Game/Assets/Scripts/NextLevel.js
+++ Maze Game/Assets/Scripts/NextLevel.js
@@ -1,4 +1,5 @@
 ﻿static var level : int = 1;
+static var titleStyle : GUIStyle;
 
 function Start() {
 	GoToNextLevel();
@@ -8,4 +9,12 @@ function GoToNextLevel() {
 	yield WaitForSeconds(5);
 	level++;
 	Application.LoadLevel("Level" + level);	
+}
+
+function OnGUI() {
+	titleStyle = new GUIStyle();
+	titleStyle.font = Resources.Load("windows_command_prompt", Font);
+	titleStyle.normal.textColor = Color.white;
+	titleStyle.fontSize = 64;
+	GUI.Label (Rect ((Screen.width / 2) - (Screen.width / 15), 8 * Screen.height / 10, Screen.width, Screen.height), PlayerMove.updatedTime, titleStyle);
 }
--- Maze Game/Assets/Scripts/PlayerMove.js
+++ Maze Game/Assets/Scripts/PlayerMove.js
@@ -1,10 +1,53 @@
 ﻿static var speed : int = 3;
+static var time : float;
+static var updatedTime : String;
+static var titleStyle : GUIStyle;
+     
+function Start() {
+	time = 0.0;
+	updatedTime = "";
+}	
      
 function Update () {
-			
-	// Debug.Log(transform.position);	
+	time += Time.deltaTime;
+	
+	var minutes = Mathf.FloorToInt(time / 60);
+	var seconds = 0;
+	var milliseconds = "";
+	var index = -1;
 		
-	// Debug.Log("HERE");
+	if (minutes > 0) {
+		seconds = Mathf.Floor(time - minutes * 60);
+		
+		var newSeconds = seconds.ToString();
+		
+		if (newSeconds.Length == 1) {
+		
+			newSeconds = "0" + newSeconds;
+		
+		}	
+			
+		milliseconds = (time - minutes * 60).ToString();
+		
+		index = milliseconds.IndexOf(".");
+		
+		milliseconds = milliseconds.Substring(index + 1, 2);
+		
+		updatedTime = minutes + ":" + newSeconds + "." + milliseconds;		
+		// Debug.Log(updatedTime);		
+	} else {
+		seconds = Mathf.FloorToInt(time);
+		
+		milliseconds = time.ToString();
+		
+		index = milliseconds.IndexOf(".");
+		
+		milliseconds = milliseconds.Substring(index + 1, 2);
+		
+		updatedTime = seconds + "." + milliseconds;
+		// Debug.Log(updatedTime);
+	}
+	
 	if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) {
 		// Debug.Log("UP");
 		transform.Translate (Vector3(0,1,0) * Time.deltaTime*speed);
@@ -18,4 +61,12 @@ function Update () {
 		// Debug.Log("RIGHT");
 		transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
 	}		
+}
+
+function OnGUI () {
+	titleStyle = new GUIStyle();
+	titleStyle.font = Resources.Load("windows_command_prompt", Font);
+	titleStyle.normal.textColor = Color.white;
+	titleStyle.fontSize = 24;
+	GUI.Label (Rect (10, 10, 500, 100), updatedTime, titleStyle);
 }
