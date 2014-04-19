875c8111056a2e11aaa74d48875ea45e53d775c4
Andrew Derry-Farrell <andrewderryfarrell@users.noreply.github.com>
2014-04-19 16:57:13 -0400

    -Added scanner beam prefab
    -Added scanner beam prefab in Prefabs/Enemies.

--- Maze Game/Assets/Scripts/Enemy/ScannerBeam.cs
+++ Maze Game/Assets/Scripts/Enemy/ScannerBeam.cs
@@ -11,6 +11,15 @@ public class ScannerBeam : MonoBehaviour
     private float flickerTimer = 0;
     private float timer = 0;
     private float stopTimer = 0;
+    private Transform myTransform;
+    private Transform target;
+    private GameObject player;
+
+    void Start()
+    {
+        myTransform = transform;
+        player = GameObject.Find("Player");
+    }
 
 	// Use this for initialization
     void Update()
@@ -27,6 +36,7 @@ public class ScannerBeam : MonoBehaviour
         if(isShowingBeam == false && stopTimer >= secondsToNotShow)
         {
             this.renderer.enabled = true;
+            this.collider2D.enabled = true;
             stopTimer = 0;
             timer = 0;
             flickerTimer = 0;
@@ -39,6 +49,7 @@ public class ScannerBeam : MonoBehaviour
             if (timer >= secondsToShow)
             {
                 this.renderer.enabled = false;
+                this.collider2D.enabled = false;
                 stopTimer = 0;
                 timer = 0;
                 flickerTimer = 0;
@@ -53,10 +64,19 @@ public class ScannerBeam : MonoBehaviour
                 else
                 {
                     this.renderer.enabled = true;
+                    this.renderer.enabled = true;
                 }
                 flickerTimer = 0;
             }
         }
 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Application.LoadLevel("CaughtScreen");
+        }
     }
 }
